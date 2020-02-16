b5690828c04f31aa7b39058a4285ec0bf5ddb234
mamokunni <makuel.mk@gmail.com>
2020-02-16 22:42:38 +0300

    redesign form date to 3 input fields

---
+++ js/script.js
@@ -0,0 +1,8 @@
+ function getAkanName(){
+
+   let days =["Sunday","Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+   let maleAkanNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
+   let femaleAkanNames = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
+   let myBirthday = document.getElementById("myBirthDate").value;
+   
+ }
