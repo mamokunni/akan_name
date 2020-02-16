0ccdbd613db3ab014ee43c9215114bc188cd1531
mamokunni <makuel.mk@gmail.com>
2020-02-17 01:14:41 +0300

    alert messages

--- js/script.js
+++ js/script.js
@@ -11,21 +11,20 @@ function getName() {
     var maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
     var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
     if (male.checked == false && female.checked == false) {
-        alert("Hey identify your gender");
+        alert("Please select gender.");
     }
     else if (year == "" || year.length < 4 || year.length > 4){
-        alert("Hey you need to enter a valid year!");
+        alert("Enter valid year!");
     }
     else if (month < 1 || month > 12 || month == 2 && day > 29 ){
-        alert("Hey you need to enter a valid month!");
+        alert("Enter valid month!");
     }
     else if( day < 1 || day >31) {
-        alert("Hey you need to re-enter a valid day!");
+        alert("Enter valid day!");
     }
     else if (male.checked === true) {
-        alert("Hey, " + maleNames[dob] + " is your Akan name" + " since you were born on a " + Weekdays[dob]);
-        alert("Your Akan Name is "+ maleNames[dob] + "")
+        alert("You were born on "+ Weekdays[dob] +". Your Akan Name is "+ maleNames[dob] + ".");
     } else {
-        alert("Hey, " + femaleNames[dob] + " is your Akan name" +" since you were born on a " + Weekdays[dob]);
+        alert("You were born on "+ Weekdays[dob] +". Your Akan Name is "+ femaleNames[dob] + ".");
     }
 }
